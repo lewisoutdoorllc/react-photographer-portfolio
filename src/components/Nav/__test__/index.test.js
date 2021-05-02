c33426fd97932cb63f04a714a5a100475181af0c
Cody <74194707+lewisoutdoorllc@users.noreply.github.com>
2021-05-02 14:20:42 -0400

    made some changes to test still have a bug to figure out

--- src/components/Nav/__test__/index.test.js
+++ src/components/Nav/__test__/index.test.js
@@ -1,5 +1,6 @@
+// __tests__/Nav.test.js with hard coded categories
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { render, cleanup, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Nav from '..';
 
@@ -15,32 +16,70 @@ afterEach(cleanup);
 
 describe('Nav component', () => {
   it('renders', () => {
-    render(<Nav 
+    render(<Nav
       categories={categories}
       setCurrentCategory={mockSetCurrentCategory}
       currentCategory={mockCurrentCategory}
       contactSelected={mockContactSelected}
       setContactSelected={mockSetContactSelected}
     />);
-  });
+  })
 
-  it('matches snapshot', () => {
-    const { asFragment } = render(<Nav />);
-    
+  it('matches snapshot DOM node structure', () => {
+    const { asFragment } = render(<Nav
+      categories={categories}
+      setCurrentCategory={mockSetCurrentCategory}
+      currentCategory={mockCurrentCategory}
+      contactSelected={mockContactSelected}
+      setContactSelected={mockSetContactSelected}
+    />);
+
     expect(asFragment()).toMatchSnapshot();
   });
 })
-describe('emoji is visible', () => {
-    it('inserts emoji into the h2', () => {
-        const { getByLabelText } = render(<Nav />);
 
-        expect(getByLabelText('camera')).toHaveTextContent('📸');
-    });
-})  
+describe('emoji is visible', () => {
+  it('inserts emoji into the h2', () => {
+    const { getByLabelText } = render(<Nav
+      categories={categories}
+      setCurrentCategory={mockSetCurrentCategory}
+      currentCategory={mockCurrentCategory}
+      contactSelected={mockContactSelected}
+      setContactSelected={mockSetContactSelected}
+    />);
+
+    expect(getByLabelText('camera')).toHaveTextContent('📸');
+  });
+})
+
 describe('links are visible', () => {
-    it('inserts text into the links', () => {
-        const { getByTestId } = render(<Nav />);
-        expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
-        expect(getByTestId('about')).toHaveTextContent('About me');
-    });
-  })
+  it('inserts text into the home link', () => {
+    const { getByTestId } = render(<Nav
+      categories={categories}
+      setCurrentCategory={mockSetCurrentCategory}
+      currentCategory={mockCurrentCategory}
+      contactSelected={mockContactSelected}
+      setContactSelected={mockSetContactSelected}
+    />);
+
+    expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
+    expect(getByTestId('about')).toHaveTextContent('About me');
+  });
+})
+
+describe('onClick events', () => {
+  it('calls the click handler when clicked', () => {
+    const { getByText } = render(<Nav
+      categories={categories}
+      setCurrentCategory={mockSetCurrentCategory}
+      currentCategory={mockCurrentCategory}
+      contactSelected={mockContactSelected}
+      setContactSelected={mockSetContactSelected}
+    />);
+    fireEvent.click(getByText('About me'))
+    fireEvent.click(getByText('Contact'))
+    fireEvent.click(getByText('Portraits'))
+
+    expect(mockSetContactSelected).toHaveBeenCalledTimes(3);
+  });
+})
